bc1b6174bfe3e231a27afd12f14ee1d101017b07
Ari Sela <arigsela@gmail.com>
2024-05-05 12:31:13 -0400

    refactoring backend, creating merge action

--- backend/database.py
+++ backend/database.py
@@ -5,13 +5,15 @@ from sqlalchemy.orm import sessionmaker, relationship
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 import models
 
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_PORT = os.environ.get("DB_PORT", "3306")
-DB_USER = os.environ.get("DB_USER", "your_username")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "your_password")
-DB_NAME = os.environ.get("DB_NAME", "your_database")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_USER = os.getenv("DB_USER", "your_username")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "your_password")
+DB_NAME = os.getenv("DB_NAME", "your_database")
 
 SQLALCHEMY_DATABASE_URL = f"mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
